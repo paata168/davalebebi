9f821ea744130f261312e752713b6513da4450ec
paata168 <eteriabesadze0@gmail.com>
2024-10-21 13:35:31 +0400

    second commit

---
+++ clas wor/home work.py
@@ -0,0 +1,15 @@
+for i in range(50, 151):
+    print(i)
+for i in range(50, 151):
+    print(i)
+for i in range(50, 151):
+    print(i)
+for i in range(200, 501, 2):
+    print(i)
+for i in range(1, 51, 2):
+    print(i)
+surname = input("შეიტანეთ თქვენი გვარი: ")
+for letter in surname:
+    print(letter)
+# For Loop-ი გამოიყენება, როდესაც ვიცით რაოდენობა, რომლითაც უნდა გავიდეთ.
+
--- day 3/lesson 2.py
+++
@@ -1,5 +0,0 @@
-age = 13
-age_in_5_years = age + 5
-print(age_in_5_years)
-
-
